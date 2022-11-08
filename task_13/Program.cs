1ba08e2f57054539a4be5962e5efe2fac6ab041b
lyubov <lyubov.chikonina@mail.ru>
2022-11-08 15:25:28 +0300

    программа 13 задачи

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 13: Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine("Введите трехзначное число: ");
+string? number = Console.ReadLine();
+if (int.TryParse(number, out int y))
+{
+    int depth = number.Length;
+    if (y < 0) depth--;
+    if (Math.Abs(y) > 99)
+    {
+        for (int i = depth; i != 3; i--)
+        {
+            y /= 10;
+        }
+        y %= 10;
+        Console.Write($"Третья цифра введенного числа: {Math.Abs(y)}");
+    }
+    else Console.Write ("Третьей цифры нет");
+}
+else Console.Write("Введеное выражение не является числом");
